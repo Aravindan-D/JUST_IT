7474c952ee229feaeabdcbd02df46831aa900d61
Aravindan D <67466775+Aravindan-D@users.noreply.github.com>
2021-08-17 15:03:33 +0530

    Update main.dart

--- just_it/lib/main.dart
+++ just_it/lib/main.dart
@@ -24,28 +24,49 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
+        appBar:
+        PreferredSize(
+        preferredSize: Size.fromHeight(60.0),
+        child:AppBar(
           backgroundColor: Colors.greenAccent,
+
           centerTitle: true,
           title: Text('Just IT'),
-        ),
+        ),),
         body: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            //mainAxisAlignment: MainAxisAlignment,
             children: [
-              TextField(
+              SizedBox(height: 150),
+              Container(
+                padding: EdgeInsets.all(10),
+                child: TextField(
                 controller: opphone,
                 keyboardType: TextInputType.phone,
                 decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+
+                ),
                   hintText: "Enter the whatsApp Number",
+
                 ),
               ),
-              TextField(
+              ),
+              SizedBox(height: 20),
+          Container(
+            padding: EdgeInsets.all(10),
+
+            child: TextField(
                 controller: opmessage,
                 decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
                   hintText: "Enter the Message!",
                 ),
-              ),
+              ),),
+              SizedBox(height: 50),
               RaisedButton(
                 color: Colors.greenAccent,
                 onPressed: () {
